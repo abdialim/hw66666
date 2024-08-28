529f3f74ee3110938ff5296b78bb4b5df1da40b4
abdialim <167313831+abdialim@users.noreply.github.com>
2024-08-28 19:04:06 +0600

    Add files via upload

---
+++ Новая папка (29)/hw6-5mec/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Poost from './Poost'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Post Search App</h1>
+      </header>
+      <main>
+        <Poost />
+      </main>
+    </div>
+  );
+}
+
+export default App;
